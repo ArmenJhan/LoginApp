2a0f37634ce9478a28a7cfe8fe9b772ac950f146
Armen <madarmen1987@gmail.com>
2022-10-05 13:40:22 +0600

    Work on mistakes

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -12,49 +12,49 @@ class LoginViewController: UIViewController {
     @IBOutlet var userNameTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
     
-    let currentUserName = "User"
-    let currentPassword = "Password"
+    private let currentUserName = "User"
+    private let currentPassword = "Password"
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
-        welcomeVC.welcome = userNameTF.text
+        welcomeVC.user = currentUserName
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    @IBAction func logInTapped() {
+        guard userNameTF.text == currentUserName, passwordTF.text == currentPassword else {
+            showAlert(
+                title: "Sorry",
+                message: "Enter correct username or password",
+                textField: passwordTF
+            )
+            return
+        }
+        performSegue(withIdentifier: "showWelcomeVC", sender: nil)
+    }
+    
+    @IBAction func forgotAction(_ sender: UIButton) {
+        sender.tag == 0
+        ? showAlert(title: "Ooops", message: "Your username is \(currentUserName)")
+        : showAlert(title: "Ooops", message: "Your password is \(currentPassword)")
     }
     
     @IBAction func unwindLogOutTapped(for segue: UIStoryboardSegue) {
         userNameTF.text = ""
         passwordTF.text = ""
     }
-    
-    @IBAction func logInTapped() {
-        guard let userName  = userNameTF.text, let password = passwordTF.text else { return }
-        if userName != currentUserName || password != currentPassword {
-            showAlert(title: "Sorry", message: "Enter correct username or password")
-        }
-    }
-    
-    @IBAction func forgotNameAction() {
-        guard let userName  = userNameTF.text else { return }
-        if userName != currentUserName {
-            showAlert(title: "Ooops", message: "You name is User😋")
-        }
-    }
-    
-    @IBAction func forgotPasswordAction() {
-        guard let password  = passwordTF.text else { return }
-        if password != currentPassword {
-            showAlert(title: "Ooops", message: "You password is Password😇")
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        view.endEditing(true)
-    }
 }
 
 extension LoginViewController {
-    private func showAlert(title: String, message: String) {
+    private func showAlert(title: String, message: String, textField: UITextField? = nil) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style: .default)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            textField?.text = ""
+        }
         alert.addAction(okAction)
         present(alert, animated: true)
     }
--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -11,11 +11,38 @@ final class WelcomeViewController: UIViewController {
 
     @IBOutlet var welcomeLabel: UILabel!
     
-    var welcome: String?
+    var user = ""
+    
+    private let firstColor = UIColor(
+        red: 210/255,
+        green: 110/255,
+        blue: 128/255,
+        alpha: 1
+    )
+    
+    private let secondColor = UIColor(
+        red: 110/255,
+        green: 150/255,
+        blue: 230/255,
+        alpha: 1
+    )
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        welcomeLabel.text = "Welcome, \(welcome ?? "")"
+        view.addGradient(topColor: firstColor, bottomColor: secondColor)
+        welcomeLabel.text = "Welcome, \(user)"
         
+    }
+}
+
+extension UIView {
+    func addGradient(topColor: UIColor, bottomColor: UIColor) {
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradient.locations = [0.0, 1.0]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        layer.insertSublayer(gradient, at: 0)
     }
 }
